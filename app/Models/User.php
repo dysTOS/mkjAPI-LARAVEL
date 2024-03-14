6596274c78cb1efc160b13bdf32f1e41f3305803
Sams, Roland <Roland.Sams@united.com>
2024-03-14 09:26:34 +0100

    update laravel 11

--- app/Models/User.php
+++ app/Models/User.php
@@ -8,12 +8,11 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
-use NotificationChannels\WebPush\HasPushSubscriptions;
 use Spatie\Permission\Traits\HasRoles;
 
 class User extends Authenticatable
 {
-    use HasApiTokens, HasFactory, Notifiable, HasRoles, Uuid, HasPushSubscriptions;
+    use HasApiTokens, HasFactory, Notifiable, HasRoles, Uuid;
 
     /**
      * @var mixed
