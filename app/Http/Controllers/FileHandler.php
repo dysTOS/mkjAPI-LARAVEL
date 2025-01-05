9abde7bc9b05238e6787c365645ddc823b25b57c
Sams, Roland <Roland.Sams@united.com>
2025-01-05 22:59:55 +0100

    add nextcloud filesystem adapter

---
+++ app/Http/Controllers/FileHandler.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
+
+class FileHandler
+{
+    private string $pathPrefix = "";
+
+    public function __construct($pathPrefix)
+    {
+        if($pathPrefix != ""){
+            $this->pathPrefix = $pathPrefix;
+        }
+    }
+
+    public function download($filePath) {
+        return Storage::download($this->$pathPrefix . $filePath);
+    }
+}
